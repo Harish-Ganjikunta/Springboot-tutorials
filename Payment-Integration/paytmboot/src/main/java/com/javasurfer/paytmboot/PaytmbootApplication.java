bc62fba77d640c18de8c6a81b70195fe9f801ae2
Harish Varma <harishvarma546@gmail.com>
2024-12-27 20:28:00 +0530

    Paytmboot initial code commit

---
+++ Payment-Integration/paytmboot/src/main/java/com/javasurfer/paytmboot/PaytmbootApplication.java
@@ -0,0 +1,13 @@
+package com.javasurfer.paytmboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmbootApplication.class, args);
+	}
+
+}
---
+++ Payment-Integration/paytmboot/src/test/java/com/javasurfer/paytmboot/PaytmbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasurfer.paytmboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
